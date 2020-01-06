81cedc398e2c0debfe080a55fd34d93996d02c6b
Erwin Paolo Yumul <paoyumul@gmail.com>
2020-01-06 20:25:30 +0800

    Create the admin section

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="admin-new-post-page">
+        <section class="new-post-form">
+            <form @submit.prevent="onSave">
+                <AppControlInput v-model="editedPost.author">
+                    Author Name
+                </AppControlInput>
+                <AppControlInput v-model="editedPost.title">
+                    Title
+                </AppControlInput>
+                <AppControlInput v-model="editedPost.thumbnailLink">
+                    Thumbnail Link
+                </AppControlInput>
+                <AppControlInput
+                    v-model="editedPost.content"
+                    control-type="textarea"
+                >
+                    Content
+                </AppControlInput>
+                <AppButton type="submit">Save</AppButton>
+                <AppButton
+                    @click="onCancel"
+                    type="button"
+                    style="margin-left: 10px"
+                    btn-style="cancel"
+                >
+                    Cancel
+                </AppButton>
+            </form>
+        </section>
+    </div>
+</template>
+
+<script>
+import AppControlInput from '~/components/UI/AppcontrolInput';
+import AppButton from '~/components/UI/AppButton';
+
+export default {
+    components: {
+        AppControlInput,
+        AppButton,
+    },
+    data() {
+        return {
+            editedPost: {
+                author: '',
+                title: '',
+                thumbnailLink: '',
+                content: '',
+            },
+        };
+    },
+    methods: {
+        onSave() {
+            // eslint-disable-next-line
+            console.log(this.editedPost);
+        },
+        onCancel() {
+            this.$router.push('/admin');
+        },
+    },
+};
+</script>
