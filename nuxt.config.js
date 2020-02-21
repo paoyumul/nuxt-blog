acee3048f7850f8680dfc609fea5b1f908107ddd
Erwin Paolo Yumul <paoyumul@gmail.com>
2020-02-21 22:08:51 +0800

    Added Core Component Plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
     /*
      ** Plugins to load before mounting the App
      */
-    plugins: [],
+    plugins: ['~plugins/core-components.js'],
     /*
      ** Nuxt.js dev-modules
      */
